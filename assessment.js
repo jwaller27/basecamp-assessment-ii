9b3715b54f9fa21d3cfbb5b212995a8f01b2c70c
jonathanwaller <waller.j27@gmail.com>
2018-04-18 12:59:57 -0500

    finall assessment-ii

--- assessment.js
+++ assessment.js
@@ -1,10 +1,14 @@
 // #1 Create an object called 'me' that has three keys, 'firstname' 'age' and 'state'. Assign the keys the appropriate values.
 
-// CODE HERE
+var me = {
+  firstname: 'Jonathan',
+  age: 33,
+  state: 'Texas'
+};
 
 // #2 Add a new key/value pair to the me object using dot notation. The new key should be 'favecolor' and set it to your favorite color as a string
 
-//CODE HERE
+me.favecolor = 'Blue';
 
 // #3 Below is an object, 'message'. Below it, add a 'text' key using bracket notation and assign it a string of whatever you would like.
 //DON'T TOUCH THE CODE BELOW
@@ -13,32 +17,49 @@ var message = {
   userName: me.firstname
 };
 //DON'T TOUCH THE CODE ABOVE
-// CODE HERE
+message['text']='Hello';
 
 // #4 Create an object called 'adjustCount' and create two methods. One called 'upVote' and one called 'downVote'. upVote should take in a number and add one to it and downVote should take in a number and minus one from it.
 
-// CODE HERE
+var adjustCount = {
+  upVote: function (num) {
+    return num+=1;
+  },
+  downVote: function(num2){
+    return num2-=1;
+  }
+};
+
 
 // #5 Create an array called 'myFriends' with four of your friends names
 
 // CODE HERE
+var myFriends = ["Tom","Bill","Wayne","Doug"];
 
 // #6 Add a fifth friends name to the end of your myFriends array
 
 // CODE HERE
+myFriends[4]="Benjamin";
 
 // #7 Create a 'myArrayCopy' variable. It should equal the myArray variable. Use the built in JavaScript method to create a copy.
 //DON'T TOUCH THE CODE BELOW
 var myArray = [1, 2, 3, 4];
 //DON'T TOUCH THE CODE ABOVE
 // CODE HERE
+myArrayCopy = myArray.slice();
 
 // #8 Below is a array, myNumbers. Create a function called 'evensOnly' that returns the 'evensArray' of only even numbers
 //DON'T TOUCH THE CODE BELOW
 var myNumbers = [333, 1, 4, 5, 511, 34, 88, 77, 222];
 //DON'T TOUCH THE CODE ABOVE
-function evensOnly() {
+
   // CODE HERE
+  function evensOnly(){
+    var evensArray = [];
+    evensArray = myNumbers.filter(function evensOnly(val,i,arr){
+    return val%2===0;
+});
+    return evensArray;
 }
 
 // #9 Using filter(), return only your friends of the array of people below. Assign it to a variable called 'trueFriends'.
@@ -51,6 +72,14 @@ var peopleIknow = [
   { name: "Michelle", friend: false },
   { name: "Holly", friend: true }
 ];
+var trueFriends = peopleIknow.filter(function(val,i){
+  return peopleIknow[i].friend===true;
+});
+
 
 // #10 create a function called indexFinder and loop over the randomNumbers array below and return the result into a new array called 'indexes' with just their indexes. Be sure to invoke indexFinder.
 let randomNumbers = [1, 3453, 34, 456, 32, 3, 2, 0];
+
+var indexes = randomNumbers.map(function indexFinder(arr,i){
+  return (i);   
+});
